c9f7edd06e8b9ddaaad852f3499d7f6bed1adf7a
leedavi <dcl@agence-sesame.fr>
2022-06-08 12:39:32 +0200

    filter interface + .Net 4.7.2

--- Componants/Interfaces/Filter.cs
+++ Componants/Interfaces/Filter.cs
@@ -12,7 +12,7 @@ namespace OpenStore.Providers.OS_CartReview
 {
     public class Filter : Nevoweb.DNN.NBrightBuy.Components.Interfaces.FilterInterface
     {
-        public override string GetFilter(string currentFilter, NavigationData navigationData, ModSettings setting, HttpContext context)
+        public override string GetFilter(String currentFilter, NavigationData navigationData, ModSettings setting, NBrightInfo ajaxInfo)
         {
             throw new NotImplementedException();
         }
